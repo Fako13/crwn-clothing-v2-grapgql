41c67ca7df35fa20aebf19050ada8288beaf1425
Sergey Neskin <neskin.x@mail.ru>
2022-11-26 22:50:48 +0700

    added GraphQL and fix category

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,31 +2,39 @@ import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
-  const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+	const { category } = useParams();
+	const { categoriesMap, loading, error } = useContext(CategoriesContext);
+	const [products, setProducts] = useState(categoriesMap[category]);
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+	useEffect(() => {
+		setProducts(categoriesMap[category]);
+	}, [category, categoriesMap]);
 
-  return (
-    <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			{loading && <Spinner />}
+			{!error ? (
+				<Fragment>
+					<Title>{category.toUpperCase()}</Title>
+					<CategoryContainer>
+						{products &&
+							products.map((product) => (
+								<ProductCard key={product.id} product={product} />
+							))}
+					</CategoryContainer>
+				</Fragment>
+			) : (
+				<h2>{error.message}</h2>
+			)}
+		</Fragment>
+	);
 };
 
 export default Category;
